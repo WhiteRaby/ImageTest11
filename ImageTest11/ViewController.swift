7b3d77b4bfe370ab5209e20287ea1ebc0f78a8fb
Alexander Golovashchenko <alexander.golovashchenko@banuba.com>
2020-02-14 20:09:28 +0300

    Initial Commit

---
+++ ImageTest11/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageTest11
+//
+//  Created by Alexander Golovashchenko on 2/14/20.
+//  Copyright © 2020 Alexander Golovashchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
